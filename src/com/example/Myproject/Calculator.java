0e31890a02e606f616bc8f1bc35f33d3884a5170
Rupkumar <rupkumargroupofnetwork@gmail.com>
2022-07-18 11:21:40 +0530

    commiting try

--- src/com/example/Myproject/Calculator.java
+++ src/com/example/Myproject/Calculator.java
@@ -1,58 +1,187 @@
 package com.example.Myproject;
-import java.util.*;
-import java.io.*;
-public class Calculator {
-    public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        int num1,num2;
-        int option;
-        int result;
-        char ch;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.function.Function;
 
-        System.out.println("\n\n		Welcome to this Mini Calculator");
+public class Calculator implements ActionListener{
+
+    JFrame frame;
+    JTextField textfield;
+    JButton[] numberButtons = new JButton[10];
+    JButton[] functionButtons = new JButton[9];
+    JButton addButtom,subButton,mulButton,divButton;
+    JButton decButton, equButton,delButton,clrButton,negButton;
+    JPanel panel;
+
+    Font myFont = new Font("Ink Free",Font.BOLD,30);
+
+    double num1 =0,num2=0,result=0;
+    char operator;
+
+    Calculator(){
+        frame = new JFrame("Calculator");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(420,550);
+        frame.setLayout(null);
+
+        textfield = new JTextField();
+        textfield.setBounds(50, 25, 300, 50);
+        textfield.setFont(myFont);
+        // textfield.setEditable(false);
+
+        addButtom = new JButton("+");
+        subButton = new JButton("-");
+        mulButton = new JButton("*");
+        divButton = new JButton("/");
+        decButton = new JButton(".");
+        equButton = new JButton("=");
+        delButton = new JButton("del");
+        clrButton = new JButton("clr");
+        negButton = new JButton("neg");
+
+        functionButtons[0] = addButtom;
+        functionButtons[1] = subButton;
+        functionButtons[2] = mulButton;
+        functionButtons[3] = divButton;
+        functionButtons[4] = decButton;
+        functionButtons[5] = equButton;
+        functionButtons[6] = delButton;
+        functionButtons[7] = clrButton;
+        functionButtons[8] = negButton;
+
+        for(int i=0; i<9; i++)
+        {
+            functionButtons[i].addActionListener(this);
+            functionButtons[i].setFont(myFont);
+            functionButtons[i].setFocusable(false);
+        }
+
+        for(int i=0; i<10; i++)
+        {
+            numberButtons[i] = new JButton(String.valueOf(i));
+            numberButtons[i].addActionListener(this);
+            numberButtons[i].setFont(myFont);
+            numberButtons[i].setFocusable(false);
+        }
+        delButton.setBounds(50,430,100,50);
+        clrButton.setBounds(150,430,100,50);
+        negButton.setBounds(250,430,100,50);
+
+        panel = new JPanel();
+        panel.setBounds(50,100, 300, 300);
+        panel.setLayout(new GridLayout(4,4,10,10));
+
+        panel.add(numberButtons[1]);
+        panel.add(numberButtons[2]);
+        panel.add(numberButtons[3]);
+        panel.add(addButtom);
+        panel.add(numberButtons[4]);
+        panel.add(numberButtons[5]);
+        panel.add(numberButtons[6]);
+        panel.add(subButton);
+        panel.add(numberButtons[7]);
+        panel.add(numberButtons[8]);
+        panel.add(numberButtons[9]);
+        panel.add(mulButton);
+        panel.add(decButton);
+        panel.add(numberButtons[0]);
+        panel.add(equButton);
+        panel.add(divButton);
+
+        frame.add(panel);
+        frame.add(negButton);
+        frame.add(delButton);
+        frame.add(clrButton);
+        frame.add(textfield);
+        frame.setVisible(true);
+    }
 
 
-        do{
+    public static void main(String[] args) {
+        Calculator cal = new Calculator();
+    }
 
-            System.out.print("\n		Enter your first number : ");
-            num1 = sc.nextInt();
-            System.out.print("\n		Enter your second number : ");
-            num2 = sc.nextInt();
 
-            System.out.println("\n		You can select one of the below options");
-            System.out.println("		1. Addition");
-            System.out.println("		2. Substration");
-            System.out.println("		3. Multiplication");
-            System.out.println("		4. Division");
-            System.out.print("\n		Plese select your option : ");
-            option = sc.nextInt();
+    @Override
+    public void actionPerformed(ActionEvent e) {
 
-            if(option == 1){
-                result = num1+num2;
-                System.out.println("\n		Addition of "+num1 +" & "+ num2+" = " + result);
+        for(int i=0; i<10; i++)
+        {
+            if(e.getSource() == numberButtons[i])
+            {
+                textfield.setText(textfield.getText().concat(String.valueOf(i)));
             }
-            else if(option == 2){
-                result = num1-num2;
-                System.out.println("\n		Substraction of "+num1 +" & "+ num2+" = " + result);
+        }
+        if(e.getSource() == decButton)
+        {
+            textfield.setText(textfield.getText().concat("."));
+        }
+        if(e.getSource() == addButtom)
+        {
+            num1 = Double.parseDouble(textfield.getText());
+            operator = '+';
+            textfield.setText("");
+        }
+        if(e.getSource() == subButton)
+        {
+            num1 = Double.parseDouble(textfield.getText());
+            operator = '-';
+            textfield.setText("");
+        }
+        if(e.getSource() == mulButton)
+        {
+            num1 = Double.parseDouble(textfield.getText());
+            operator = '*';
+            textfield.setText("");
+        }
+        if(e.getSource() == divButton)
+        {
+            num1 = Double.parseDouble(textfield.getText());
+            operator = '/';
+            textfield.setText("");
+        }
+
+        if(e.getSource() == equButton)
+        {
+            num2 = Double.parseDouble(textfield.getText());
+
+            switch(operator)
+            {
+                case '+':
+                    result = num1+num2;
+                    break;
+                case '-':
+                    result = num1-num2;
+                    break;
+                case '*':
+                    result = num1*num2;
+                    break;
+                case '/':
+                    result = num1/num2;
+                    break;
             }
-            else if(option == 3){
-                result = num1*num2;
-                System.out.println("\n		Multiplication of "+num1 +" & "+ num2+" = " + result);
+            textfield.setText(String.valueOf(result));
+            num1 = result;
+        }
+        if(e.getSource() == clrButton)
+        {
+            textfield.setText("");
+        }
+        if(e.getSource() == delButton)
+        {
+            String string = textfield.getText();
+            textfield.setText("");
+            for(int i=0; i<string.length()-1; i++)
+            {
+                textfield.setText(textfield.getText()+string.charAt(i));
             }
-            else if(option == 4){
-                result = num1/num2;
-                System.out.println("\n		Division of "+num1 +" & "+ num2+" = " + result);
-            }
-            else{
-                System.out.println("\n		Invalid Choice");
-            }
-
-            System.out.print("\n		Do you want to continue(y/n) : ");
-            ch = sc.next().charAt(0);
-
-        }while(ch == 'y' || ch == 'Y');
-
-
-
+        }
+        if(e.getSource() == negButton)
+        {
+            double temp = Double.parseDouble(textfield.getText());
+            temp *= -1;
+            textfield.setText(String.valueOf(temp));
+        }
     }
 }
